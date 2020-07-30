5a5708f6cf917f92e22cd592673600db759da16a
Nina Utrata <nina.utrata@idego.pl>
2020-07-30 14:42:19 +0200

    Adding Actor model in models.py

--- mysite/filmapp/models.py
+++ mysite/filmapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Actors(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=200)
+    birthday = models.DateTimeField()
+
