46971ebd6b5af8369867259dc49f430806d2fbd1
Nina Utrata <nina.utrata@idego.pl>
2020-07-30 16:52:59 +0200

    Still updating models.py

--- mysite/filmapp/admin.py
+++ mysite/filmapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Actors, Directors, Movies
+from .models import Actors, Directors, Movies,Ratings
 
 # Register your models here.
 admin.site.register(Actors)
 admin.site.register(Directors)
 admin.site.register(Movies)
+admin.site.register(Ratings)
--- mysite/filmapp/models.py
+++ mysite/filmapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=200)
     birthday = models.DateField()
+
+    def __str__(self):
+        return self.name+' '+self.surname
 
     class Meta:
         abstract = True
@@ -17,9 +21,17 @@ class Directors(Person):
 class Movies(models.Model):
     title = models.CharField(max_length=300)
     plot = models.CharField(max_length=1000)
+    date = models.DateField(default=datetime.now())
     actors = models.ManyToManyField(Actors)
     directors = models.ManyToManyField(Directors)
 
+    def year_from_date(self):
+        return self.date.strftime('%Y')
+
+    def __str__(self):
+        return self.title+' ('+str(self.year_from_date())+') '
+
 class Ratings(models.Model):
+    actor = models.OneToOneField(Actors, on_delete=models.CASCADE)
     stars = models.SmallIntegerField()
 
