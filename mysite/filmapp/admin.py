0296fd9170cca8edb4eb304263882f95dfbc15df
Nina Utrata <nina.utrata@idego.pl>
2020-07-30 15:35:12 +0200

    Updating models.py

--- mysite/filmapp/admin.py
+++ mysite/filmapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Actors
+from .models import Actors, Directors, Movies
 
 # Register your models here.
 admin.site.register(Actors)
+admin.site.register(Directors)
+admin.site.register(Movies)
--- mysite/filmapp/models.py
+++ mysite/filmapp/models.py
@@ -1,8 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Actors(models.Model):
+class Person(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=200)
-    birthday = models.DateTimeField()
+    birthday = models.DateField()
+
+    class Meta:
+        abstract = True
+
+class Actors(Person):
+    pass
+class Directors(Person):
+    pass
+
+class Movies(models.Model):
+    title = models.CharField(max_length=300)
+    plot = models.CharField(max_length=1000)
+    actors = models.ManyToManyField(Actors)
+    directors = models.ManyToManyField(Directors)
+
+class Ratings(models.Model):
+    stars = models.SmallIntegerField()
 
