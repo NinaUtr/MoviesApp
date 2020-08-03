a185e581dfb9b061029e467ab14d057adfc7aff3
Nina Utrata <nina.utrata@idego.pl>
2020-08-03 09:02:18 +0200

    Creating basic views for lists of actors, dircetors, movies

--- mysite/moviesapp/urls.py
+++ mysite/moviesapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloWorld, name='helloWorld'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-]
+    path('actors/', views.all_actors, name='actors'),
+    path('directors/', views.all_directors, name='directors'),
+    path('movies/', views.all_movies, name='movies')]
--- mysite/moviesapp/views.py
+++ mysite/moviesapp/views.py
@@ -2,12 +2,32 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Actors, Directors, Movies
+from django.shortcuts import render
+
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
+def all_actors(request):
+    try:
+        actors = Actors.object.all()
+        return render(request, 'main_content/actors.html', {'actors': [actors]})
+    except AttributeError:
+        return render(request, 'main_content/actors.html', {'actors': []})
 
-def helloWorld(request):
-    return HttpResponse('Hello World')
+def all_directors(request):
+    try:
+        directors = Directors.object.all()
+        return render(request, 'main_content/directors.html', {'directors': [directors]})
+    except AttributeError:
+        return render(request, 'main_content/directors.html', {'directors': []})
+
+def all_movies(request):
+    try:
+        movies = Movies.object.all()
+        return render(request, 'main_content/movies.html', {'movies': [movies]})
+    except AttributeError:
+        return render(request, 'main_content/movies.html', {'movies': []})
